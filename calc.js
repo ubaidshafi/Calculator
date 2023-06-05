53269501084168cb0134fff3bc619cc651d3880c
ubaidshafi <ubaidshafi009@gmail.com>
2023-06-05 23:33:46 +0530

    calc

---
+++ calc.js
@@ -0,0 +1,32 @@
+// // display
+// function display(num){
+//      output.value += num;  
+//     //  id:value=to get value
+// }
+
+// // clear AC
+// function clearAll(){
+//     output.value=""
+// }
+
+// // evalute
+
+
+// function eval(){
+
+// method 1:
+// exp=output.value; exp=7+7
+// result=eval(exp);result=14
+// output.value=result;display=14
+//     // method 2:
+//     output.value =eval(output.value);
+
+// }
+
+
+// // remove last item
+
+// function removeLast(){
+//     currentExp=output.value;
+//     output.value=currentExp.slice(0,-1);
+// }
